518601f24d7347befdca1f4a264e94e04bd5604c
nolan-h-hamilton <nhh.git@gmail.com>
2025-08-03 22:02:17 -0400

    support py311

--- setup.py
+++ setup.py
@@ -30,8 +30,8 @@ setup(
     include_package_data=True,
     package_dir={"": "src"},
     ext_modules=cythonize(extensions, language_level="3"),
-    install_requires=["cython>=3.0", "numpy>=2.2.0", "pandas>=2.0", "scipy>=1.15.0",
+    install_requires=["cython>=3.0", "numpy>=2.1", "pandas>=2.3", "scipy>=1.15",
                       "pysam>=0.23.3", "pybedtools>=0.11.2"],
-    python_requires=">=3.10",
+    python_requires=">=3.11",
     zip_safe=False,
 )
