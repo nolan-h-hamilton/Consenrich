22bf3e5cd6d523f2032b2b073663dc48310780fd
nolan-h-hamilton <nhh.git@gmail.com>
2025-08-03 21:50:12 -0400

    attempt support for 3.10

--- setup.py
+++ setup.py
@@ -30,8 +30,8 @@ setup(
     include_package_data=True,
     package_dir={"": "src"},
     ext_modules=cythonize(extensions, language_level="3"),
-    install_requires=["cython>=3.0", "numpy>=2.3.0", "pandas>=2.0", "scipy>=1.16.0",
+    install_requires=["cython>=3.0", "numpy>=2.2.0", "pandas>=2.0", "scipy>=1.15.0",
                       "pysam>=0.23.3", "pybedtools>=0.11.2"],
-    python_requires=">=3.12",
+    python_requires=">=3.10",
     zip_safe=False,
 )
