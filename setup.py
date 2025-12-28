1d78764fa11a06249e73cb391fd8a66887323569
nolan-h-hamilton <nhh.git@gmail.com>
2025-12-28 16:25:57 -0500

    bump version

--- setup.py
+++ setup.py
@@ -48,7 +48,7 @@ extensions = [
 
 setup(
     name="consenrich",
-    version="0.7.19rc1",
+    version="0.7.20rc1",
     packages=find_packages(where="src"),
     package_dir={"": "src"},
     include_package_data=True,
--- src/consenrich/__init__.py
+++ src/consenrich/__init__.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 
-__version__ = "0.7.19rc1"
+__version__ = "0.7.20rc1"
 from importlib import import_module
 
 cconsenrich = import_module(__name__ + ".cconsenrich")
