5da22fed81fc55b045b5092d7b33c940d6fc142a
nolan-h-hamilton <nhh.git@gmail.com>
2026-01-02 03:09:13 -0500

    bump version

--- setup.py
+++ setup.py
@@ -48,7 +48,7 @@ extensions = [
 
 setup(
     name="consenrich",
-    version="0.7.23rc1",
+    version="0.8.0rc1",
     packages=find_packages(where="src"),
     package_dir={"": "src"},
     include_package_data=True,
